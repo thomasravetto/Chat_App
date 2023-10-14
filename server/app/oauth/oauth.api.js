82bf66fd5fa3efab62d132079d03571200d9e938
ThomasRavetto <travetto18@gmail.com>
2023-10-14 21:41:49 +0200

    initial commit for oauth

---
+++ server/app/oauth/oauth.api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const oauthRouter = express.Router();
+
+// const { loginWithOauth } = require('./oauth.controller');
+
+// oauthRouter.get('/auth/google', loginWithOauth);
+
+module.exports = oauthRouter;
