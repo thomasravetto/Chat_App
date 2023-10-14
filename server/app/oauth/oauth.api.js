831aaafdb49f78288f74cb9343d7c5f39e469754
ThomasRavetto <travetto18@gmail.com>
2023-10-14 21:55:15 +0200

    initializing oauth structure

--- server/app/oauth/oauth.api.js
+++ server/app/oauth/oauth.api.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const oauthRouter = express.Router();
 
-// const { loginWithOauth } = require('./oauth.controller');
+const { googleAuth, googleCallback, googleLogout, googleFailure } = require('./oauth.controller');
 
-// oauthRouter.get('/auth/google', loginWithOauth);
+oauthRouter.get('/auth/google', googleAuth);
+
+oauthRouter.get('/auth/google/callback', googleCallback);
+
+oauthRouter.get('/auth/logout', googleLogout);
+
+oauthRouter.get('/failure', googleFailure);
 
 module.exports = oauthRouter;
---
+++ server/app/oauth/oauth.controller.js
@@ -0,0 +1,17 @@
+const {  } = require('../../helpers/oauth/oauth.helper');
+
+function googleAuth (req, res) {
+    // TODO
+}
+
+function googleCallback (req, res) {
+    // TODO
+}
+
+function googleLogout (req, res) {
+    // TODO
+}
+
+function googleFailure (req, res) {
+    // TODO
+}
