6f28f5a8f7c1beaf58dd68626e880ac4bd246323
ThomasRavetto <travetto18@gmail.com>
2023-10-18 11:35:52 +0200

    adding signin form

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
+
 import LoginAndRegister from './components/authentication/LoginAndRegister';
 import './App.css';
 
 function App() {
+
+  const [isActive, setIsActive] = useState(false);
+
+    const handleClick = (event) => {
+        setIsActive(current => !current);
+    }
+
   return (
     <div className="App">
-      <div className='container' id='container'>
-        <LoginAndRegister/>
+      <div className={`container ${isActive ? 'right-panel-active' : ''}`} id='container'>
+        <LoginAndRegister handleClick={handleClick}/>
       </div>
     </div>
   );
--- client/src/components/authentication/LoginAndRegister.js
+++ client/src/components/authentication/LoginAndRegister.js
@@ -1,47 +1,49 @@
-function Login() {
+function Login(props) {
     return (
         <div className='login_and_register_form'>
-            <div class="form-container sign-up-container">
+            <div className="form-container sign-up-container">
                 <form action="#">
                     <h1>Create Account</h1>
-                    <div class="social-container">
-                        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                    <div className="social-container">
+                        <a href="https://localhost:3500/v1/auth/google" className="google-btn">
+                            <img className="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                            <p className="btn-text"><b>Sign up with Google</b></p>
+                        </a>
                     </div>
                     <span>or use your email for registration</span>
-                    <input type="text" placeholder="Name" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <input className='input_form' type="text" placeholder="Name" />
+                    <input className='input_form' type="email" placeholder="Email" />
+                    <input className='input_form' type="password" placeholder="Password" />
                     <button>Sign Up</button>
                 </form>
             </div>
-            <div class="form-container sign-in-container">
+            <div className="form-container sign-in-container">
                 <form action="#">
                     <h1>Sign in</h1>
-                    <div class="social-container">
-                        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                    <div className="social-container">
+                        <a href="https://localhost:3500/v1/auth/google" className="google-btn">
+                            <img className="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                            <p className="btn-text"><b>Continue with Google</b></p>
+                        </a>
                     </div>
                     <span>or use your account</span>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <input className='input_form' type="email" placeholder="Email" />
+                    <input className='input_form' type="password" placeholder="Password" />
                     <a href="#">Forgot your password?</a>
                     <button>Sign In</button>
                 </form>
             </div>
-            <div class="overlay-container">
-                <div class="overlay">
-                    <div class="overlay-panel overlay-left">
+            <div className="overlay-container">
+                <div className="overlay">
+                    <div className="overlay-panel overlay-left">
                         <h1>Welcome Back!</h1>
                         <p>To keep connected with us please login with your personal info</p>
-                        <button class="ghost" id="signIn">Sign In</button>
+                        <button className="ghost" id="signIn" onClick={props.handleClick}>Sign In</button>
                     </div>
-                    <div class="overlay-panel overlay-right">
+                    <div className="overlay-panel overlay-right">
                         <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button class="ghost" id="signUp">Sign Up</button>
+                        <p>Enter your personal details and start your journey with us</p>
+                        <button className="ghost" id="signUp" onClick={props.handleClick}>Sign Up</button>
                     </div>
                 </div>
             </div>
