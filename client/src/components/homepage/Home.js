141d458c9fa315c86e6a5946834acafac11a536d
ThomasRavetto <travetto18@gmail.com>
2023-10-30 19:38:39 +0100

    updating homepage component

--- client/src/components/homepage/Home.js
+++ client/src/components/homepage/Home.js
@@ -1,9 +1,15 @@
 import { useState } from 'react';
+import appLogo from '../../logo_chat_app.png';
 
 function Home (props) {
 
+    const [username, setUsername] = useState(props.username);
+    const [email, setEmail] = useState(props.email);
+
     return (
-        <div>{props.username} {props.email}</div>
+        <div className='navbar_container'>
+            <img className='logo_image' src={appLogo} alt='Logo'></img>
+        </div>
     )
 }
 
