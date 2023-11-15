bdf7c805dd77bc1ab8998c46e3fa27504a438cbd
ThomasRavetto <travetto18@gmail.com>
2023-11-15 10:20:50 +0100

    friends functions

--- server/app/friends/friends.controller.js
+++ server/app/friends/friends.controller.js
@@ -3,11 +3,15 @@ const express = require('express');
 const { getFriendsOfUserIdHelper } = require('../../helpers/friends/friends.helper');
 
 async function getFriendsOfUserId (req, res) {
-    const { userId } = req.body;
+    try {
+        const { userId } = req.body;
 
-    const friendsList = await getFriendsOfUserIdHelper(userId);
+        const friendsList = await getFriendsOfUserIdHelper(userId);
 
-    res.status(200).json(friendsList);
+        res.status(200).json(friendsList);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
 }
 
 module.exports = {
