4ab8385c3d6451a46b43879236a5da6f88062290
ThomasRavetto <travetto18@gmail.com>
2023-11-17 18:10:28 +0100

    adding socket

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const fs = require('fs');
 const https = require('https');
+const { Server } = require('socket.io');
 
 const appRouter = require('./appRouter');
 
@@ -11,7 +12,31 @@ const options = {
     cert: fs.readFileSync('./cert.pem')
 }
 
-const server = https.createServer(options, appRouter);
+const server = https.createServer(options, appRouter); // Creating HTTPS server
+
+const io = new Server(server); // Creating Socket server
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('send_message', (data) => {
+        const {chatId, message} = data;
+        socket.to(chatId).emit('receive_message', {
+            message: message
+        });
+    })
+
+    socket.on('join', (room) => {
+        socket.join(room);
+        socket.to(room).emit('user_connected', {
+            message: `User connected to chat: ${room}`
+        });
+    });
+});
 
 function startServer () {
     server.listen(PORT, () => {
@@ -20,3 +45,5 @@ function startServer () {
 }
 
 startServer();
+
+module.exports = { io }; // Exporting Socket server
